e23713c4ac7a54964e9e5947ba88673a062026de
YohannesC01 <yohannessigan2@gmail.com>
2025-08-19 12:55:37 +0800

    lab7

--- labs/lab07/exercise2/shopping_data.py
+++ labs/lab07/exercise2/shopping_data.py
@@ -5,12 +5,5 @@ product_name = "Laptop"
 price = 3500.00   
 quantity = 2
 
-
 # Calculate total cost
 total_cost = price * quantity
-
-# Display the result
-print("Product:", product_name)
-print("Price per item: RM", price)
-print("Quantity:", quantity)
-print("Total cost: RM", total_cost)
