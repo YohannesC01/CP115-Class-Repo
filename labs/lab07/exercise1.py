a59a23d3c13388e4c36fc98cdbe980d8e6ec362d
YohannesC01 <yohannessigan2@gmail.com>
2025-08-19 13:03:54 +0800

    lab7

--- labs/lab07/exercise1.py
+++ labs/lab07/exercise1.py
@@ -4,13 +4,6 @@ product_name = "Laptop"
 price = 1299.99
 quantity = 2
 total = price * quantity
-
-# Old way (convert these to f-strings)
-print("Product: " + product_name)
-print("Price: $" + str(price))
-print("Quantity: " + str(quantity))
-print("Total: $" + str(total))
-
 #New way
 print(f"Product: {product_name}")
 print(f"Price: ${price}")
