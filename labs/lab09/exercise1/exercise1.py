667caf1604bb68eff1413d546229516b27f064b5
YohannesC01 <yohannessigan2@gmail.com>
2025-09-02 12:00:34 +0800

    lab09

--- labs/lab09/exercise1/exercise1.py
+++ labs/lab09/exercise1/exercise1.py
@@ -5,6 +5,15 @@ gpa = float(input("Enter GPA (0.0-4.0): "))
 credit_hours = int(input("Enter credit hours: "))
 
 # TODO your code here
+if gpa >= 3.8 and credit_hours >= 12
+    classification = "Dean's_List"
+elif gpa >= 3.5 and credit_hours >= 12
+    classification = "Honor Roll"
+elif gpa >= 2.0
+    classification = "Good Standing"
+elif gpa <= 2.0 
+    classification = "Academic Probation"
+    
 
 # Display results
 print(f"\nStudent: {student_name}")
