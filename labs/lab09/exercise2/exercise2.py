6dfcea84a0ce30b6b6ff7a722075fe4a38e13974
YohannesC01 <yohannessigan2@gmail.com>
2025-09-02 13:17:08 +0800

    lab-09

--- labs/lab09/exercise2/exercise2.py
+++ labs/lab09/exercise2/exercise2.py
@@ -4,7 +4,24 @@ overtime_hours = int(input())
 tax_status = input()
 
 # TODO your code here
-if tax_status == single and base_salary >= 5000:
+if tax_status == "single" and base_salary >= 5000:
+    tax_rate = 22
+else:
+    tax_rate = 18
+
+if tax_status == "married" and base_salary >= 6000:
+    tax_rate = 20
+else:
+    tax_rate =  15
+
+if tax_status == "head" and base_salary >= 5500:
+    tax_rate = 25
+else:
+    tax_rate = 19
+
+EPF = 11/100
+SOCSO = 0.05/100
+net_salary = ((tax_rate + EPF + SOCSO) * base_salary)  + (35 * overtime_hours)
 
 
 
