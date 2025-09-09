1a65206090dc72960dfb22daaef9a902ed3f77ba
YohannesC01 <yohannessigan2@gmail.com>
2025-09-09 12:08:53 +0800

    lab9

--- labs/lab09/exercise2/exercise2.py
+++ labs/lab09/exercise2/exercise2.py
@@ -5,22 +5,25 @@ tax_status = input()
 
 # TODO your code here
 if tax_status == "single" and base_salary >= 5000:
-    tax_rate = 22
-else:
-    tax_rate = 18
+    tax_rate = 0.22
+
+elif tax_status == "married" and base_salary <= 5000:
+    tax_rate = 0.18
 
 if tax_status == "married" and base_salary >= 6000:
-    tax_rate = 20
-else:
-    tax_rate =  15
+    tax_rate = 0.20
+
+elif tax_status == "single" and base_salary <= 6000:
+    tax_rate =  0.15
 
 if tax_status == "head" and base_salary >= 5500:
-    tax_rate = 25
-else:
-    tax_rate = 19
+    tax_rate = 0.25
+
+elif tax_status == "head" and base_salary <= 5500:
+    tax_rate = 0.19
 
 EPF = 11/100
-SOCSO = 0.05/100
+SOCSO = 5/100
 net_salary = ((tax_rate + EPF + SOCSO) * base_salary)  + (35 * overtime_hours)
 
 
