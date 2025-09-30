b4bf187c906094650081a70b30e29a438b26bc48
YohannesC01 <yohannessigan2@gmail.com>
2025-09-30 13:07:34 +0800

    lab11

--- labs/lab11/exercise2/exercise2.py
+++ labs/lab11/exercise2/exercise2.py
@@ -1,8 +1,18 @@
 num_days = int(input())
 danger_threshold = float(input())
-
+danger_days = 0
+total_temperatures = 0
 # TODO: Your code here
 # Use input() inside the loop to get each day's temperature
+for days_num in range(num_days):
+    temperatures = float(input())
+    total_temperatures += temperatures
+
+    if temperatures > danger_threshold:
+        danger_days += 1
+    
+
+average_temp = total_temperatures / num_days
 
 print(danger_days)
 print(f"{average_temp:.1f}")
--- labs/lab11/exercise3/exercise3.py
+++ labs/lab11/exercise3/exercise3.py
@@ -3,5 +3,6 @@ target_points = int(input())
 # TODO: Your code here
 # Use input() inside the while loop to get points each round
 
+
 print(total_points)
 print(rounds_played)
